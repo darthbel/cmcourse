469e9d9d52c70bafc4b4acd9612be66c02d0d475
darthbel <febelgine@gmail.com>
2019-02-02 11:48:15 -0500

    Testing first domain class

--- src/main/java/com/felipebelgine/cmcourse/resources/CategoryResource.java
+++ src/main/java/com/felipebelgine/cmcourse/resources/CategoryResource.java
@@ -1,16 +1,28 @@
 package com.felipebelgine.cmcourse.resources;
 
+import com.felipebelgine.cmcourse.domain.Category;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "/categories")
 public class CategoryResource {
 
     @RequestMapping(method = RequestMethod.GET)
-    public String list() {
-        return "Rest is working!";
+    public List<Category> list() {
+
+        Category cat1 = new Category(1, "IT");
+        Category cat2 = new Category(1, "Office");
+
+        List<Category> list = new ArrayList<Category>();
+        list.add(cat1);
+        list.add(cat2);
+
+        return list;
     }
 
 }
