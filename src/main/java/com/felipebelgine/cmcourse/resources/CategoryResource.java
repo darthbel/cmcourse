6ba186e7de34f97c14fd245a3cac48e3a3d1c56a
darthbel <febelgine@gmail.com>
2019-02-02 12:29:10 -0500

    Creating Category Repository and Service

--- src/main/java/com/felipebelgine/cmcourse/resources/CategoryResource.java
+++ src/main/java/com/felipebelgine/cmcourse/resources/CategoryResource.java
@@ -1,28 +1,26 @@
 package com.felipebelgine.cmcourse.resources;
 
 import com.felipebelgine.cmcourse.domain.Category;
+import com.felipebelgine.cmcourse.services.CategoryService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.ArrayList;
-import java.util.List;
 
 @RestController
 @RequestMapping(value = "/categories")
 public class CategoryResource {
 
-    @RequestMapping(method = RequestMethod.GET)
-    public List<Category> list() {
+    @Autowired
+    private CategoryService service;
 
-        Category cat1 = new Category(1, "IT");
-        Category cat2 = new Category(1, "Office");
-
-        List<Category> list = new ArrayList<Category>();
-        list.add(cat1);
-        list.add(cat2);
-
-        return list;
+    @RequestMapping(value = "/{id}", method = RequestMethod.GET)
+    public ResponseEntity<?> find(@PathVariable Integer id) {
+        Category obj = service.find(id);
+        return ResponseEntity.ok().body(obj);
     }
 
 }
---
+++ src/main/java/com/felipebelgine/cmcourse/services/CategoryService.java
@@ -0,0 +1,20 @@
+package com.felipebelgine.cmcourse.services;
+
+import com.felipebelgine.cmcourse.domain.Category;
+import com.felipebelgine.cmcourse.repositories.CategoryRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class CategoryService {
+
+    @Autowired
+    private CategoryRepository repo;
+
+    public Category find(Integer id) {
+        Optional<Category> obj = repo.findById(id);
+        return obj.orElse(null);
+    }
+}
