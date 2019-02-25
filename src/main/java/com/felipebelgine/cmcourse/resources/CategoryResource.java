96160908ce53faf3e6be61b3a25e0b690e029e0f
darthbel <febelgine@gmail.com>
2019-02-25 14:29:02 -0500

    Listing all categories using DTO

--- src/main/java/com/felipebelgine/cmcourse/resources/CategoryResource.java
+++ src/main/java/com/felipebelgine/cmcourse/resources/CategoryResource.java
@@ -1,6 +1,7 @@
 package com.felipebelgine.cmcourse.resources;
 
 import com.felipebelgine.cmcourse.domain.Category;
+import com.felipebelgine.cmcourse.dto.CategoryDTO;
 import com.felipebelgine.cmcourse.services.CategoryService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -8,6 +9,8 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import java.net.URI;
+import java.util.List;
+import java.util.stream.Collectors;
 
 
 @RestController
@@ -42,5 +45,12 @@ public class CategoryResource {
     public ResponseEntity<Void> delete(@PathVariable Integer id) {
         service.delete(id);
         return ResponseEntity.noContent().build();
+    }
+
+    @RequestMapping(method = RequestMethod.GET)
+    public ResponseEntity<List<CategoryDTO>> findAll() {
+        List<Category> list = service.findAll();
+        List<CategoryDTO> listDto = list.stream().map(obj -> new CategoryDTO(obj)).collect(Collectors.toList());
+        return ResponseEntity.ok().body(listDto);
     }
 }
